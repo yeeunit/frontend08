6e7741b47eb00335270b01d3a206ed7feabd0256
yeeunit <yeeunit22@gmail.com>
2022-07-14 18:02:06 +0900

    프로그래머스

--- programmers/2W_Thur/2w_t_1.js
+++ programmers/2W_Thur/2w_t_1.js
@@ -2,7 +2,7 @@
 // 자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
 // 예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
 
-
+// 내코드 정답
 
 function solution(n)
 {
@@ -14,4 +14,33 @@ function solution(n)
     }
 
     return answer;
+}
+
+
+// 정답코드
+
+function solution(n)
+{   
+    n = String(n)
+    // "숫자는 인덱스값으로 접근 불가하기 때문에 문자로 변환"
+    // num.toString()
+    let answer = 0;
+    
+    for (let i=0; i< n.length; i++){
+        answer += Number(n[i])
+    }
+    return answer;
+}
+
+// reduce 사용하기 (배열에서만 사용 가능한 메소드)
+
+function solution(n){
+    const answer 
+    = String(n).split('').reduce((acc, cur)=>{  
+        return acc + Number(cur)
+    }, 0) //1일떄 오류발생.. 따라서 초기값 0 지정
+    return answer
+}
+
+
 }
--- programmers/2W_Thur/2w_t_2.js
+++ programmers/2W_Thur/2w_t_2.js
@@ -2,7 +2,7 @@
 
 // 함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다. 다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
 
-
+// 내답 정답
 function solution(x, n) {
     
     let answer = [];
@@ -12,4 +12,24 @@ function solution(x, n) {
     }    
     return answer;
 }
+
+// 정답 코드
+
+function solution(x, n) {
+    const answer =[]
+    for(let i=1; i<=n; i++){
+        answer.push(i*x)
+        //console.log(i*x)
+    }
+    return answer
+}
+
+// 모든 결과를 배열에 넣어보기 (map)
+
+function solution(x, n) {     // n개만큼 배열값 생성, 1로 채워보기
+    const answer = new Array(n).fill(1).map((num, i)=>
+    (num+i)*x)
+    return answer
+}
+
 
