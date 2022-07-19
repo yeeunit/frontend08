1beefbe3f92e112d164ebd55b3eee5e91a933823
yeeunit <yeeunit22@gmail.com>
2022-07-19 17:55:16 +0900

    프로그래머스 풀이

--- programmers/3W_Tue/1.js
+++ programmers/3W_Tue/1.js
@@ -9,6 +9,35 @@ function solution(n) {
     for(let i=0; i<String(n).length; i++){
         answer.push(String(n).length-i)
     }
-        
     return answer;
 }
+
+// 수업 코드
+
+function solution(n) {
+    const answer = []
+    n = n.toString()
+    // console.log(type)
+
+    for(let i=n.length-1; i>=0; i--){
+        // console.log(n[i], i)
+        answer.push(Number(n[i]))
+    }
+    return answer
+}
+
+// 메서드로 풀기
+
+function solution(n) {
+    const answer = n.toString().split('').reverse().map(num =>{
+            // console.log(answer)
+            return Number(Number)
+    })
+    return answer
+}
+
+// 
+// for(let ) 포문 그대로
+// answer.push(Numver(n[i]))
+// answer.reverse()
+// return answer
--- programmers/3W_Tue/2.js
+++ programmers/3W_Tue/2.js
@@ -1,6 +1,6 @@
 // 나누어 떨어지는 숫자 배열
 
-
+// 내답
 function solution(arr, divisor) {
     let answer = [];
     
@@ -13,4 +13,29 @@ function solution(arr, divisor) {
     	}
     }
   return answer
+}
+
+
+// 수업 !!! 리턴 뒤 확인
+
+function solution(arr, divisor) {
+  const answer = [];
+  for(let i=0; i<arr.length; i++)
+      // console.log(arr[i], arr[i]%divisor)
+    {
+    if(arr[i]%divisor === 0){
+      answer.push(arr[i])
+    }
+  }
+  return answer.length === 0? [-1] : answer.sort((a,b)=>a-b)
+
+}
+
+// 
+function solution(arr, divisor) {
+  const answer = arr.filter(num => {
+    return num%divisor === 0
+  })
+  // console.log(answer)
+  return answer.length === 0 [-1] : answer.sort((a,b)=> a-b)
 }
